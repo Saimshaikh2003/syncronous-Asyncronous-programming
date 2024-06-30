d57d06999d0d274d0f36ebb753640e51ea83bd96
Saimshaikh112 <160286046+Saimshaikh2003@users.noreply.github.com>
2024-06-30 14:38:54 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,128 @@
+// // // let a:number = 10
+// // // let b:number = 20
+// // // setTimeout(() => {
+// // //     console.log(a);
+// // //     console.log(b);
+    
+    
+// // // }, 1000);
+
+
+
+
+
+
+
+
+
+
+// // // let ab:number = 100
+// // // let abc :number = 200
+
+// // // setTimeout(() => {
+// // //     console.log(ab);
+// // //     console.log(abc);
+    
+    
+// // // }, 2000);
+
+// // // ab = 300
+// // // abc= 400
+
+
+
+
+
+// // // let bc:number = 20
+// // // let bd:number = 30
+
+
+// // // setTimeout(()=>{
+// // //     console.log(bc);
+// // //     console.log(bd);
+    
+    
+// // // }, 4000)
+
+
+
+
+
+
+// // let Married:boolean = true
+// // setTimeout(()=>{
+// //     if(Married){
+// //         console.log("yes saim is married");    
+// //     }
+// //     else{
+// //         console.log("saim is single");
+        
+// //     }
+// // }, 3000)
+
+
+
+
+
+
+
+
+
+
+
+
+// let be:number =20
+
+// setTimeout(() => {
+//     if(be <=20){
+//         console.log("This is same number");
+        
+//     }else{
+//         console.log("This is not same number");
+        
+//     }
+    
+    
+// },2000);
+
+
+
+// let s:number=10
+
+// setTimeout(() => {
+//     if(s >=11){
+//         console.log("Big");
+        
+//     }
+//     else{
+//         console.log(("small"));
+        
+//     }
+    
+// },3000);
+
+
+//Synronous programming
+
+let a = 20
+let b= 21
+console.log(a);
+console.log(b);
+
+//Asyncronous programming
+
+console.log(11);
+
+console.log(12);
+
+setTimeout(() => {
+    console.log(13);
+    
+},3000);
+
+for(let i= 1 ; i<=10; i++){
+    console.log(`This  is saim ${i}`);
+    
+}
+
+
